484b89c8b0c5a8da883cca230fd7e3d1146f9b15
vaishali86c <vaishali86c@gmail.com>
2023-06-07 20:03:50 +0530

    Add File

--- 07-LinkedList/src/practice1/Main.java
+++ 07-LinkedList/src/practice1/Main.java
@@ -1,7 +1,34 @@
 package practice1;
 
+import Practice.linkedlist;
+
 public class Main {
     public static void main(String[] args) {
+//        linkedlist1 list  = new linkedlist1();
+//
+//        list.insertFirst(9);
+//        list.insertFirst(8);
+//        list.insertFirst(7);
+//        list.insertFirst(6);
+//        list.insertFirst(5);
+//        list.insertFirst(4);
+//        list.insertFirst(3);
+//        list.display();
+//        list.insertLast(10);
+//        list.display();
+//        list.insert(11,3);
+//        list.display();
+//        list.deleteFirst();
+//        list.display();
+        DLL list  = new DLL();
+//
+        list.insertFirst(9);
+        list.insertFirst(8);
+        list.insertFirst(7);
+        list.insertFirst(6);
+        list.insertFirst(5);
+        list.display();
+
 
     }
 }
--- 07-LinkedList/src/practice1/linkedlist1.java
+++ 07-LinkedList/src/practice1/linkedlist1.java
@@ -1,32 +1,94 @@
 package practice1;
 
-public class linkedlist1 {
+import Practice.linkedlist;
 
+public class linkedlist1 {
 
     private Node head;
     private Node tail;
 
     private int size;
 
-    public void linkedlist1() {
-        this.size = 0;
+    public linkedlist1() {
+        this.size= 0;
     }
 
+    public void insertFirst (int val) {
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+        if (tail == null) {
+            tail = head;
+        }
+
+        size += 1;
+
+    }
+
+    public void insertLast (int val){
+
+        if (tail == null) {
+            insertFirst(val);
+            return;
+        }
+        Node node = new Node(val);
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+
+    public void insert(int val,int index) {
+        if (index == 0) {
+            insertFirst(val);
+            return;
+        }
+        if (index == size) {
+            insertLast(val);
+            return;
+        }
+        Node temp = head;
+        for (int i = 1; i < index; i++) {
+            temp = temp.next;
+        }
+        Node node = new Node(val, temp.next);
+        temp.next = node;
+
+        size++;
+    }
+
+    public int deleteFirst () {
+        int val = head.value;
+        head = head.next;
+
+        if (head == null){
+            tail = null;
+        }
+        size--;
+        return val;
+    }
+
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
 
     private class Node {
-
         private int value;
-
         private Node next;
 
-        private void Node(int value) {
-            this.value = 0;
+        public Node (int value) {
+            this.value = value;
         }
 
-        private void Node (int value,Node next){
-            this.value = 0;
-            this.next = next;
-
+        public Node (int value,Node next) {
+            this.value = value;
+            this.next= next;
         }
     }
+
 }
